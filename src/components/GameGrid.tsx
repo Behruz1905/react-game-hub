d2e65cc8f03d255621be50c773d76aab015bc609
Bahruz Ibrahimov <bahruzibrahimov@Bahruzs-MacBook-Pro.local>
2025-01-20 23:10:24 +0400

    add a dynamic heading

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,13 +3,10 @@ import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
 import { GameQuery } from "../App";
 
 interface Props {
   gameQuery: GameQuery;
-  // selectedGenre: Genre | null;
-  // selectedPlatform: Platform | null;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
