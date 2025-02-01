d21cc0b6b7976af83e8d82e2123b35cda715fca0
Bahruz Ibrahimov <bahruzibrahimov@Bahruzs-MacBook-Pro.local>
2025-02-02 02:34:26 +0400

    refactor simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from 'ms';
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -38,7 +39,7 @@ const  useGames = (gameQuery: GameQuery) =>
       return lastPage.next ? allPages.length + 1 : undefined;
     },
     initialPageParam: 1,  
-    staleTime: 24 * 60 * 60 * 1000, // 24h
+    staleTime: ms('24h')
   })
   
   
